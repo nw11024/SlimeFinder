12c1f24ad1b86ea257afe3f6be06b0aa131fca54
KamiSkidder <106670735+nw11024@users.noreply.github.com>
2022-06-18 17:48:31 +0900

    基本的な部分を変更

---
+++ src/main/java/com/zest/slimefinder/SlimeFinder.java
@@ -0,0 +1,14 @@
+package net.zest.slimefinder;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlimeFinder implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("slimefinder");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
